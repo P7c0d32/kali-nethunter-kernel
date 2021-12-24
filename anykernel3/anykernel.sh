6a8c1e5430c6812a8d7abcade20675786e6426b5
flypatriot <flypatriotka@gmail.com>
2021-12-24 14:50:29 +0300

    Made some fixes

--- anykernel3/anykernel.sh
+++ anykernel3/anykernel.sh
@@ -6,7 +6,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NetHunter Kernel for the OnePlus 7 Pro
+kernel.string=NetHunter Kernel for the OnePlus 8 series
 do.devicecheck=1
 do.modules=1
 do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
